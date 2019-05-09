7388f2f7c69354143469d8cfdcd21538082f2fb5
Ranjit <ranjitshinde91@gmail.com>
2019-05-10 00:05:03 +0530

    add validatin for movie id

--- src/main/java/com/ranjit/shinde/springbootdemo/advice/RestResponseExceptionEntityHandler.java
+++ src/main/java/com/ranjit/shinde/springbootdemo/advice/RestResponseExceptionEntityHandler.java
@@ -3,22 +3,30 @@ package com.ranjit.shinde.springbootdemo.advice;
 import com.ranjit.shinde.springbootdemo.exception.MovieNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.jws.WebResult;
+import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
 public class RestResponseExceptionEntityHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler(MovieNotFoundException.class)
-    protected ResponseEntity<Object> handleConflict(RuntimeException exception,
+    protected ResponseEntity<Object> handleConflict(MovieNotFoundException exception,
                                                     WebRequest request){
 
         return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
 
+    }
+
+    @ExceptionHandler(value = { ConstraintViolationException.class })
+    public ResponseEntity<String> handleConflict(ConstraintViolationException ex, WebRequest request) {
+        String message = ex.getMessage()+ " cannot be empty";
+        return new ResponseEntity < > (message, null, HttpStatus.BAD_REQUEST);
     }
 }
--- src/main/java/com/ranjit/shinde/springbootdemo/controller/MovieController.java
+++ src/main/java/com/ranjit/shinde/springbootdemo/controller/MovieController.java
@@ -6,15 +6,21 @@ import com.ranjit.shinde.springbootdemo.model.Movie;
 import com.ranjit.shinde.springbootdemo.service.MovieService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/movies")
+@Validated
 @Slf4j
 public class MovieController {
 
@@ -29,7 +35,7 @@ public class MovieController {
 
     @ResponseBody
     @GetMapping("/{id}")
-    public Movie byId(@PathVariable("id") String id){
+    public Movie byId(@PathVariable("id") @NotBlank @Size(min = 2) String id ){
        try {
         return  movieService.get(id);
        }
