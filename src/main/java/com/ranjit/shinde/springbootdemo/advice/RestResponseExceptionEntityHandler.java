d61d7845e3ac23e68ced3f3b0f8bdc6b1812f77d
Ranjit <ranjitshinde91@gmail.com>
2019-05-09 23:13:42 +0530

    rename exception handler file

--- src/main/java/com/ranjit/shinde/springbootdemo/advice/RestResponseExceptionEntityHandler.java
+++ src/main/java/com/ranjit/shinde/springbootdemo/advice/RestResponseExceptionEntityHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import javax.jws.WebResult;
 
 @ControllerAdvice
-public class RestResponseExceptionEntitiHandler extends ResponseEntityExceptionHandler {
+public class RestResponseExceptionEntityHandler extends ResponseEntityExceptionHandler {
 
 
     @ExceptionHandler(MovieNotFoundException.class)
