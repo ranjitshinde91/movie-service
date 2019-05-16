53b1851fe334e80397d1cf6d299f39098918cb04
Ranjit <ranjitshinde91@gmail.com>
2019-05-16 23:21:07 +0530

    fix build fail and run test

--- src/main/java/com/ranjit/shinde/springbootdemo/advice/RestResponseExceptionEntityHandler.java
+++ src/main/java/com/ranjit/shinde/springbootdemo/advice/RestResponseExceptionEntityHandler.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.jws.WebResult;
 import javax.validation.ConstraintViolationException;
 
 @ControllerAdvice
