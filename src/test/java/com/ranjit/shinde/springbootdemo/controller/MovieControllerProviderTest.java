c2aae03f868812e1cc6a6ab172f8118fbea5f292
Ranjit <ranjitshinde91@gmail.com>
2019-05-20 16:04:21 +0530

    add pact contracts

---
+++ src/test/java/com/ranjit/shinde/springbootdemo/controller/MovieControllerProviderTest.java
@@ -0,0 +1,53 @@
+package com.ranjit.shinde.springbootdemo.controller;
+
+
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.RestPactRunner;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.target.MockMvcTarget;
+import com.ranjit.shinde.springbootdemo.model.Movie;
+import com.ranjit.shinde.springbootdemo.service.MovieService;
+import org.junit.Before;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+@RunWith(RestPactRunner.class)
+@Provider("MovieService")// same as in the "provider_name" part in our pact file
+@PactFolder("pact/")
+public class MovieControllerProviderTest {
+
+    @Mock
+    private MovieService movieService;
+
+    private MovieController movieController;
+
+    @TestTarget
+    public final MockMvcTarget target = new MockMvcTarget();
+
+
+    @Before
+    public void before() {
+        initMocks(this);
+        movieController = new MovieController(movieService);
+        target.setControllers(movieController);
+    }
+
+    @State("Test Get") // same as the "given()" part in our consumer test
+    public void personData() {
+        Movie movie = new Movie("LOTR",
+                LocalDate.of(2006,05, 26), 123);
+        when(movieService.get(anyString())).thenReturn(movie);
+    }
+
+}
